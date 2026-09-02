f66cf34f276f0be998c96f9d49e8dbad54afbd76
Rahul <ranarahul62000@gmail.com>
2026-09-02 18:51:07 +0530

    Fix GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/grandstay-hotel-management-system/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
